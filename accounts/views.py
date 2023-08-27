bc8594b47e9b69e2e2059c8b3738383d8b759110
put your github username here <YOUR EMAIL ADDRESS>
2023-08-27 20:07:45 +0000

    add customUser model

--- accounts/views.py
+++ accounts/views.py
@@ -1,2 +1,6 @@
-from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
+from django.shortcuts import render, get_object_or_404
+from .models import CustomUser
+
+def profile_view(request, slug):  
+    user = get_object_or_404(CustomUser, slug=slug)
+    return render(request, 'account/profile.html', {'user': user})
